8a870ffd8ebead0d2d279879224589edb0788ba3
choi-jae-hyuck <cgear@naver.com>
2018-09-17 16:27:08 +0900

    move_down()완료
    아랫방향 이동완료

--- Drills/Drill-05/drill-05.py
+++ Drills/Drill-05/drill-05.py
@@ -28,6 +28,13 @@ def move_up():
     pass
 
 def move_down():
+    x, y = 0 + 25, 600 - 50
+    while y > 90:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y -= 2
+        delay(0.01)
     pass
 
 def move_left():
@@ -46,7 +53,7 @@ def move_right():
 def make_rectangle():
     # move_from_center_to_right()
     # move_up()
-    move_left()
+    # move_left()
     move_down()
     move_right()
     pass
